d55df0cdd424007298eeb85b0910d5b93dfd3066
sidambhire <sca54@cwru.edu>
2022-11-01 22:20:31 -0400

    Default Mathematica directory state

--- Kernel/init.m
+++ Kernel/init.m
@@ -1,11 +1,2 @@
 (** User Mathematica initialization file **)
-Notation`AutoLoadNotationPalette = False
-Needs["Notation`"]
-
-Notation[ParsedBoxWrapper[SubscriptBox["t_", "i_"]] \[DoubleLongLeftRightArrow] 
-   ParsedBoxWrapper[RowBox[{"Indexed", "[", 
-      RowBox[{"t_", ",", RowBox[{"List", "[", "i_", "]"}]}], 
-      "]"}]]]
-
-Alert[]:=EmitSound[Sound[SoundNote[]]]
 
